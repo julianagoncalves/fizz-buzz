08265d40a00247baf4920c5244d537c447453f70
Juliana Gonçalves <julianagoncalves.jgp@gmail.com>
2015-10-03 19:35:41 -0300

    Separando arquivo somente com o método fizzBuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from os import system
 
+def fizzBuzz(numero):
 
-class GameFizzBuzz:
+    if(numero % 3 == 0 and numero % 5 == 0):
+        return "Fizz Buzz"
 
-    def __init__(self):
-        self.fizzBuzzMenu()
+    if(numero % 3 == 0):
+        return "Fizz"
 
-    def fizzBuzzMenu(self):
-        system("clear")
-        print("____________________________________ Fizz Buzz _____________________________________")
-        print("\nFizz -> Múltiplo de 3 | Buzz -> Múltiplo de 5 | Fizz Buzz -> Múltiplo de 3 e 5")
-        print("\n____________________________________________________________________________________")
+    if(numero % 5 == 0):
+        return "Buzz"
 
-        numero = input("\nInsira um número: ")
-
-        print ("\n----> ") + str(self.fizzBuzz(numero))
-
-    def fizzBuzz(self, numero):
-
-        if(numero % 3 == 0 and numero % 5 == 0):
-            return "Fizz Buzz"
-
-        if(numero % 3 == 0):
-            return "Fizz"
-
-        if(numero % 5 == 0):
-            return "Buzz"
-        return numero
-
-if __name__ == '__main__':
-    GameFizzBuzz()
+    return numero
